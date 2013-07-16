d6528f22e71c2c7cdcfc39143196919105c96b3e
Saxon Landers <saxon@ackwell.com.au>
2013-07-16 18:29:36 +1000

    That wasn't the best idea i've had recently. Reverting most of it.

--- static/js/bbcode-tags.js
+++ static/js/bbcode-tags.js
@@ -1,53 +1,4 @@
-function MarkdownBoldBBCode() {
-	this.getCodeName = function() { return 'Bold'; }
-	this.getDisplayName = function() { return 'b'; }
-	this.needsEnd = function() { return true; }
-	this.canHaveCodeContent = function() { return true; }
-	this.canHaveArgument = function() { return false; }
-	this.mustHaveArgument = function() { return false; }
-	this.getAutoCloseCodeOnOpen = function() { return null; }
-	this.getAutoCloseCodeOnClose = function() { return null; }
-	this.isValidArgument = function(settings, argument) { return false; }
-	this.isValidParent = function(settings, parent) { return true; }
-	this.escape = function(settings, content) { return PHPC.htmlspecialchars(content); }
-	this.open = function(settings, argument, closingCode) { return '**'; }
-	this.close = function(settings, argument, closingCode) { return '**'; }
-}
-MarkdownBoldBBCode.prototype = new BBCode;
 
-function MarkdownItalicBBCode() {
-	this.getCodeName = function() { return 'Italic'; }
-	this.getDisplayName = function() { return 'i'; }
-	this.needsEnd = function() { return true; }
-	this.canHaveCodeContent = function() { return true; }
-	this.canHaveArgument = function() { return false; }
-	this.mustHaveArgument = function() { return false; }
-	this.getAutoCloseCodeOnOpen = function() { return null; }
-	this.getAutoCloseCodeOnClose = function() { return null; }
-	this.isValidArgument = function(settings, argument) { return false; }
-	this.isValidParent = function(settings, parent) { return true; }
-	this.escape = function(settings, content) { return PHPC.htmlspecialchars(content); }
-	this.open = function(settings, argument, closingCode) { return '*'; }
-	this.close = function(settings, argument, closingCode) { return '*'; }
-}
-MarkdownItalicBBCode.prototype = new BBCode;
-
-function MarkdownStrikeThroughBBCode() {
-	this.getCodeName = function() { return 'StrikeThrough'; }
-	this.getDisplayName = function() { return 's'; }
-	this.needsEnd = function() { return true; }
-	this.canHaveCodeContent = function() { return true; }
-	this.canHaveArgument = function() { return false; }
-	this.mustHaveArgument = function() { return false; }
-	this.getAutoCloseCodeOnOpen = function() { return null; }
-	this.getAutoCloseCodeOnClose = function() { return null; }
-	this.isValidArgument = function(settings, argument) { return false; }
-	this.isValidParent = function(settings, parent) { return true; }
-	this.escape = function(settings, content) { return PHPC.htmlspecialchars(content); }
-	this.open = function(settings, argument, closingCode) { return '~~'; }
-	this.close = function(settings, argument, closingCode) { return '~~'; }
-}
-MarkdownStrikeThroughBBCode.prototype = new BBCode;
 
 function MarkdownQuoteBBCode() {
 	this.getCodeName = function() { return 'Quote'; }
@@ -66,7 +17,7 @@ function MarkdownQuoteBBCode() {
 }
 MarkdownQuoteBBCode.prototype = new BBCode;
 
-function MarkdownCodeBBCode() {
+function CodeBBCode() {
 	this.getCodeName = function() { return 'Code'; }
 	this.getDisplayName = function() { return 'code'; }
 	this.needsEnd = function() { return true; }
@@ -78,48 +29,12 @@ function MarkdownCodeBBCode() {
 	this.isValidArgument = function(settings, argument) { return true; }
 	this.isValidParent = function(settings, parent) { return true; }
 	this.escape = function(settings, content) { return PHPC.htmlspecialchars(content); }
-	this.open = function(settings, argument, closingCode) { return '`'; }
-	this.close = function(settings, argument, closingCode) { return '`'; }
+	this.open = function(settings, argument, closingCode) { return '<code>'; }
+	this.close = function(settings, argument, closingCode) { return '</code>'; }
 }
-MarkdownCodeBBCode.prototype = new BBCode;
+CodeBBCode.prototype = new BBCode;
 
-var MarkdownCodeBoxBBCode = MarkdownCodeBBCode;
-
-function MarkdownLinkBBCode() {
-	this.getCodeName = function() { return 'Link'; }
-	this.getDisplayName = function() { return 'url'; }
-	this.needsEnd = function() { return true; }
-	this.canHaveCodeContent = function() { return true; }
-	this.canHaveArgument = function() { return true; }
-	this.mustHaveArgument = function() { return true; }
-	this.getAutoCloseCodeOnOpen = function() { return null; }
-	this.getAutoCloseCodeOnClose = function() { return null; }
-	this.isValidArgument = function(settings, argument) { return true; }
-	this.isValidParent = function(settings, parent) { return parent !== this.getDisplayName(); }
-	this.escape = function(settings, content) { return PHPC.htmlspecialchars(content); }
-	this.open = function(settings, argument, closingCode) { return '['; }
-	this.close = function(settings, argument, closingCode) { return ']('+PHPC.htmlspecialchars(argument)+')'; }
-}
-MarkdownLinkBBCode.prototype = new BBCode;
-
-function MarkdownImageBBCode() {
-	this.getCodeName = function() { return 'Image'; }
-	this.getDisplayName = function() { return 'img'; }
-	this.needsEnd = function() { return true; }
-	this.canHaveCodeContent = function() { return false; }
-	this.canHaveArgument = function() { return true; }
-	this.mustHaveArgument = function() { return false; }
-	this.getAutoCloseCodeOnOpen = function() { return null; }
-	this.getAutoCloseCodeOnClose = function() { return null; }
-	this.isValidArgument = function(settings, argument) { return true; }
-	this.isValidParent = function(settings, parent) { return true; }
-	this.escape = function(settings, content) { return PHPC.htmlspecialchars(content); }
-	this.open = function(settings, argument, closingCode) {
-		return '!['+PHPC.htmlspecialchars(argument)+'](';
-	}
-	this.close = function(settings, argument, closingCode) { return ')'; }
-}
-MarkdownImageBBCode.prototype = new BBCode;
+var CodeBoxBBCode = CodeBBCode;
 
 function YouTubeBBCode() {
 	this.getCodeName = function() { return 'YouTube'; }
@@ -144,14 +59,9 @@ YouTubeBBCode.prototype = new BBCode;
 var bbcode = new BBCodeParser({
 	escapeContentOutput: false
 , codes: {
-		'b': new MarkdownBoldBBCode()
-	, 'i': new MarkdownItalicBBCode()
-	, 's': new MarkdownStrikeThroughBBCode()
-	, 'quote': new MarkdownQuoteBBCode()
-	, 'code': new MarkdownCodeBBCode()
-	, 'codebox': new MarkdownCodeBoxBBCode()
-	, 'url': new MarkdownLinkBBCode()
-	, 'img': new MarkdownImageBBCode()
+		'quote': new MarkdownQuoteBBCode()
+	, 'code': new CodeBBCode()
+	, 'codebox': new CodeBoxBBCode()
 	, 'yt': new YouTubeBBCode()
 	}
 });
