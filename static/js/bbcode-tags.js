9cd42781694fb53d43a2e452311fdf596e8f1f7e
Saxon Landers <saxon@ackwell.com.au>
2013-10-22 16:39:11 +1100

    Fiddling with BBCode

--- static/js/bbcode-tags.js
+++ static/js/bbcode-tags.js
@@ -1,4 +1,23 @@
-
+function HTMLLinkBBCode() {
+	this.getCodeName = function() { return 'Link'; }
+	this.getDisplayName = function() { return 'url'; }
+	this.needsEnd = function() { return true; }
+	this.canHaveCodeContent = function() { return true; }
+	this.canHaveArgument = function() { return true; }
+	this.mustHaveArgument = function() { return true; }
+	this.getAutoCloseCodeOnOpen = function() { return null; }
+	this.getAutoCloseCodeOnClose = function() { return null; }
+	this.isValidArgument = function(settings, argument) { return true; }
+	this.isValidParent = function(settings, parent) { return parent !== this.getDisplayName(); }
+	this.escape = function(settings, content) { return PHPC.htmlspecialchars(content); }
+	this.open = function(settings, argument, closingCode) {
+		return '<a href="' + PHPC.htmlspecialchars(argument) + '">';
+	}
+	this.close = function(settings, argument, closingCode) {
+		return '</a>';
+	}
+}
+HTMLLinkBBCode.prototype = new BBCode;
 
 function MarkdownQuoteBBCode() {
 	this.getCodeName = function() { return 'Quote'; }
@@ -21,7 +40,7 @@ function CodeBBCode() {
 	this.getCodeName = function() { return 'Code'; }
 	this.getDisplayName = function() { return 'code'; }
 	this.needsEnd = function() { return true; }
-	this.canHaveCodeContent = function() { return false; }
+	this.canHaveCodeContent = function() { return true; }
 	this.canHaveArgument = function() { return true; }
 	this.mustHaveArgument = function() { return false; }
 	this.getAutoCloseCodeOnOpen = function() { return null; }
@@ -33,8 +52,6 @@ function CodeBBCode() {
 	this.close = function(settings, argument, closingCode) { return '</code>'; }
 }
 CodeBBCode.prototype = new BBCode;
-
-var CodeBoxBBCode = CodeBBCode;
 
 function YouTubeBBCode() {
 	this.getCodeName = function() { return 'YouTube'; }
@@ -55,13 +72,32 @@ function YouTubeBBCode() {
 }
 YouTubeBBCode.prototype = new BBCode;
 
+// Spoiler tag
+function SpoilerBBCode() {
+	this.getCodeName = function() { return 'Spoiler'; }
+	this.getDisplayName = function() { return 'spoiler'; }
+	this.needsEnd = function() { return true; }
+	this.canHaveCodeContent = function() { return false; }
+	this.canHaveArgument = function() { return false; }
+	this.mustHaveArgument = function() { return false; }
+	this.getAutoCloseCodeOnOpen = function() { return null; }
+	this.getAutoCloseCodeOnClose = function() { return null; }
+	this.isValidParent = function(settings, parent) { return true; }
+	this.escape = function(settings, content) { return PHPC.htmlspecialchars(content); }
+	this.open = function(settings, argument, closingCode) { return '<span class="spoiler">'; }
+	this.close = function(settings, argument, closingCode) { return '</span>'; }
+}
+SpoilerBBCode.prototype = new BBCode;
+
 // Line up the BBCode parser.
 var bbcode = new BBCodeParser({
 	escapeContentOutput: false
 , codes: {
-		'quote': new MarkdownQuoteBBCode()
+		'a': new HTMLLinkBBCode()
+	, 'quote': new MarkdownQuoteBBCode()
 	, 'code': new CodeBBCode()
-	, 'codebox': new CodeBoxBBCode()
+	, 'codebox': new CodeBBCode()
 	, 'yt': new YouTubeBBCode()
+	, 'spoiler': new SpoilerBBCode()
 	}
 });
